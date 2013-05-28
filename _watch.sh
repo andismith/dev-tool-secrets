35c80f246f4423f3ca355c9a07a858a6a98c02af
andismith <andi.smith.uk@gmail.com>
2013-05-28 23:55:56 +0100

    Add SASS watch script

---
+++ _watch.sh
@@ -0,0 +1,7 @@
+# _watch.sh
+
+sass --watch assets/scss/styles.scss:assets/css/styles.min.css --style compressed --sourcemap
+#sass --watch assets/scss/styles.scss:assets/css/styles.css --style expanded
+
+exit 0
+
